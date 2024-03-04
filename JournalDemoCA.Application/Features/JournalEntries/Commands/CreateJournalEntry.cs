8a5734bf92f097efd58a6f3fda232d581aa814cd
Danipan497 <danipan497@gmail.com>
2024-03-04 20:06:44 +0100

    JournalDemoCA.Application/Features/JournalEntries/Commands CreateJournalEntry class and ICreateJournalEntry interface created.

---
+++ JournalDemoCA.Application/Features/JournalEntries/Commands/CreateJournalEntry.cs
@@ -0,0 +1,32 @@
+﻿using JournalDemoCA.Domain.Common.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JournalDemoCA.Application.Features.JournalEntries.Commands
+{
+    public class CreateJournalEntry : ICreateJournalEntry
+    {
+        private List<JournalEntry> entry = new();
+        public CreateJournalEntry()
+        {
+            entry.Add(new JournalEntry { Id = 1, FirstName = "Daniel", LastName = "Pandza" });
+            entry.Add(new JournalEntry { Id = 2, FirstName = "Paniel", LastName = "Dandza" });
+        }
+
+        public List<JournalEntry> GetJournalEntries()
+        {
+            return entry;
+        }
+
+        public JournalEntry InsertEntry(string firstName, string lastName)
+        {
+            JournalEntry j = new() { FirstName = firstName, LastName = lastName };
+            j.Id = entry.Max(x => x.Id) + 1;
+            entry.Add(j);
+            return j;
+        }
+    }
+}
---
+++ JournalDemoCA.Application/Features/JournalEntries/Commands/ICreateJournalEntry.cs
@@ -0,0 +1,10 @@
+﻿using JournalDemoCA.Domain.Common.Entities;
+
+namespace JournalDemoCA.Application.Features.JournalEntries.Commands
+{
+    public interface ICreateJournalEntry
+    {
+        List<JournalEntry> GetJournalEntries();
+        JournalEntry InsertEntry(string firstName, string lastName);
+    }
+}
