ed77d9e6c47cc564779f2643de190c9665aef86c
Danipan497 <danipan497@gmail.com>
2024-03-05 18:41:59 +0100

    JournalDemoCA.WebAPI Program.cs modified and JounralEntryController controller created.

---
+++ JournalDemoCA.WebAPI/Controllers/JournalEntryController.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JournalDemoCA.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JournalEntryController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public JournalEntryController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+    }
+}
