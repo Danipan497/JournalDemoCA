ebda5d7729bb4802d2e9bdcb8f4843d6e7434277
Danipan497 <danipan497@gmail.com>
2024-03-05 19:31:23 +0100

    JournalDemoCA.Application Handler folder and handler classes created.

---
+++ JournalDemoCA.Application/Features/JournalEntries/Handlers/GetJournalEnrtyByIdHandler.cs
@@ -0,0 +1,30 @@
+﻿using JournalDemoCA.Application.Features.JournalEntries.Queries;
+using JournalDemoCA.Domain.Common.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JournalDemoCA.Application.Features.JournalEntries.Handlers
+{
+    public class GetJournalEnrtyByIdHandler : IRequestHandler<GetJournalEntryByIdQuery, JournalEntry>
+    {
+        private readonly IMediator _mediator;
+
+        public GetJournalEnrtyByIdHandler(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task<JournalEntry> Handle(GetJournalEntryByIdQuery request, CancellationToken cancellationToken)
+        {
+            var results = await _mediator.Send(new GetJournalEntryListQuery());
+            var output = results.FirstOrDefault(x => x.Id == request.Id);
+
+            return output;
+        }
+    }
+}
---
+++ JournalDemoCA.Application/Features/JournalEntries/Handlers/GetJournalEntryListHandler.cs
@@ -0,0 +1,27 @@
+﻿using JournalDemoCA.Application.Features.JournalEntries.Commands;
+using JournalDemoCA.Application.Features.JournalEntries.Queries;
+using JournalDemoCA.Domain.Common.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JournalDemoCA.Application.Features.JournalEntries.Handlers
+{
+    public class GetJournalEntryListHandler : IRequestHandler<GetJournalEntryListQuery, List<JournalEntry>>
+    {
+        private readonly ICreateJournalEntry _createJournalEntry;
+
+        public GetJournalEntryListHandler(ICreateJournalEntry createJournalEntry)
+        {
+            _createJournalEntry = createJournalEntry;
+        }
+
+        public Task<List<JournalEntry>> Handle(GetJournalEntryListQuery request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(_createJournalEntry.GetJournalEntries());
+        }
+    }
+}
